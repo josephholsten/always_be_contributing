99be1d1830d64cb0077644c1022c645cee2bc2af
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-06-15 15:24:35 +0000

    Fixes #7 by adding support for date ranges

--- lib/always_be_contributing/user.rb
+++ lib/always_be_contributing/user.rb
@@ -8,25 +8,33 @@ module AlwaysBeContributing
   class User < Struct.new(:name)
     # return a list of all Contributions available for this user
     def contributions
-      doc = contribution_url.read
-      JSON.parse(doc).map do |cont|
-        Contribution.from_raw(cont)
+      @contributions ||= begin
+        doc = contribution_url.read
+        JSON.parse(doc).map do |cont|
+          Contribution.from_raw(cont)
+        end
       end
     end
 
     # return a list of all Contributions since start_date
     def contributions_since(start_date)
+      tomorrow = Date.today + 1 # yay timezones
+      contributions_between(start_date...tomorrow)
+    end
+
+    def contributions_between(range)
       contributions.select do |c|
-        c.date >= start_date
+        range.cover? c.date
       end
     end
 
     # return the sum of the values for all Contributions since start_date
     def contribution_count_since(start_date)
-      @contribution_count_since ||= {}
-      @contribution_count_since[start_date] ||= begin
-        contributions_since(start_date).map(&:value).reduce(&:+)
-      end
+      contributions_since(start_date).map(&:value).reduce(&:+)
+    end
+
+    def contribution_count(date_range)
+      contributions_between(date_range).map(&:value).reduce(&:+)
     end
 
     private
